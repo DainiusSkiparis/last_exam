7f79862b3636df697ecf88efb290400b0cddccc1
Dainius <109749812+JenLoVangas@users.noreply.github.com>
2023-03-20 22:25:52 +0200

    Revert "Dev"

--- src/main/java/lt/codeacademy/last_exam/repositories/PlayerRepository.java
+++
@@ -1,7 +0,0 @@
-package lt.codeacademy.last_exam.repositories;
-
-import lt.codeacademy.last_exam.entities.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepository extends JpaRepository<Player, Long> {
-}
